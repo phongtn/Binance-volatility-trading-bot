d7691373e1a16a47a0a575379a7b58803e5e3e13
PhongTn <phongtn.group1@gmail.com>
2024-02-24 17:01:36 +0700

    refactor pause module

--- pausebotmod.py
+++ pausebotmod.py
@@ -4,11 +4,7 @@ import time
 
 from tradingview_ta import TA_Handler, Interval
 
-INTERVAL = Interval.INTERVAL_1_MINUTE  # Timeframe for analysis
 
-EXCHANGE = 'BINANCE'
-SCREENER = 'CRYPTO'
-SYMBOL = 'BTCUSDT'
 THRESHOLD = 7  # 7 of 15 MAs indicating sell
 TIME_TO_WAIT = 1  # Minutes to wait between analysis
 FULL_LOG = False  # List analysis result to console
@@ -19,10 +15,10 @@ def analyze():
     handler = {}
 
     handler = TA_Handler(
-        symbol=SYMBOL,
-        exchange=EXCHANGE,
-        screener=SCREENER,
-        interval=INTERVAL,
+        symbol='BTCUSDT',
+        exchange='BINANCE',
+        screener='CRYPTO',
+        interval=Interval.INTERVAL_1_MINUTE,
         timeout=10)
 
     try:
@@ -33,10 +29,10 @@ def analyze():
     ma_sell = analysis.moving_averages['SELL']
     if ma_sell >= THRESHOLD:
         paused = True
-        print(f'pause_bot_mod: Market not looking too good, bot paused from buying {ma_sell}/{THRESHOLD} '
+        print(f'pause_bot_mod: The market is not looking good, bot paused from buying {ma_sell}/{THRESHOLD} '
               f'Waiting {TIME_TO_WAIT} minutes for next market checkup')
     else:
-        print(f'pause_bot_mod: Market looks ok, bot is running {ma_sell}/{THRESHOLD} '
+        print(f'pause_bot_mod: The market is looking good, bot is running {ma_sell}/{THRESHOLD} '
               f'Waiting {TIME_TO_WAIT} minutes for next market checkup ')
         paused = False
 
