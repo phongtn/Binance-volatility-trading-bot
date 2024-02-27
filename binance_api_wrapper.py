234d265644743502327f943c8cb3d30e84388d81
PhongTn <phongtn.group1@gmail.com>
2024-02-27 07:34:17 +0700

    create a binance API wrapper

---
+++ binance_api_wrapper.py
@@ -0,0 +1,19 @@
+from binance.client import Client
+
+
+class BinanceAPIWrapper(Client):
+    def rolling_window_price_change(self, pair: str, window: str):
+        """
+        Rolling window price change statistics. Note: This endpoint is different from the GET /api/v3/ticker/24hr
+        endpoint. The window used to compute statistics will be no more than 59999ms from the requested windowSize.
+        openTime for /api/v3/ticker always starts in a minute, while the closeTime is the current time of the
+        request. As such, the effective window will be up to 59999ms wider than windowSize.
+        E.g. If the closeTime is 1641287867099 (January 04, 2022 09:17:47:099 UTC),
+        and the windowSize is 1d the openTime will be: 1641201420000 (January 3, 2022, 09:17:00 UTC)
+        https://binance-docs.github.io/apidocs/spot/en/#rolling-window-price-change-statistics
+        """
+        params = {
+            'symbol': pair,
+            'windowSize': window
+        }
+        return self._get('ticker', data=params, version=self.PRIVATE_API_VERSION)
