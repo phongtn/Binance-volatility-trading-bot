d68c5db6a03a11230d7ca1b70d60e2babe2f7f02
PhongTn <phongtn.group1@gmail.com>
2024-03-07 14:59:09 +0700

    reformat

--- binance_api_wrapper.py
+++ binance_api_wrapper.py
@@ -49,7 +49,7 @@ class BinanceAPIWrapper(Client):
         except Exception as exception:
             print(f'round price failed: {exception}')
 
-    def     get_klines_minutes(self, symbol: str, interval: str, duration: int, end=datetime.now()):
+    def get_klines_minutes(self, symbol: str, interval: str, duration: int, end=datetime.now()):
         # end = datetime.now()
         start = (end - timedelta(minutes=float(duration))).timestamp()
         return self.get_klines(symbol=symbol, interval=interval, startTime=int(start * 1000),
